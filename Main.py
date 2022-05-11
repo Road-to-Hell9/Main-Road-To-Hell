f15049ecec82ac996a00d578abe00fe7b0a4fac1
Christian Glaeser <Christian.glaeser05@gmail.com>
2022-05-11 12:09:53 +0200

    Main

--- Main.py
+++ Main.py
@@ -7,16 +7,26 @@ from PIL import ImageTk, Image
 root = Tk()
 
 #Variables
+xWmax = 870
+yWmax = 570
 
+carW = 75
+
+xplayer = 2
+yplayer = yWmax/2 - carW
 
 #Imports
 bg = PhotoImage(file = "pixel-street.png")
+player = PhotoImage(file = "main-car.png")
 
 #----------Code----------#
 root.geometry("870x570")
-# Show image using label
-label1 = Label( root, image = bg)
+
+label1 = Label(root, image = bg)
 label1.place(x = -2, y = -1)
+
+lable2 = Label(root, image = player)
+lable2.place(x = xplayer, y = yplayer)
 
 # Execute tkinter
 root.mainloop()
