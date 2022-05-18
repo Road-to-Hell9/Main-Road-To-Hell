f46e7b4113cd357d116f11b6f4fd83c62482d8c5
Christian Glaeser <Christian.glaeser05@gmail.com>
2022-05-18 12:28:18 +0200

    main

--- Main.py
+++ Main.py
@@ -15,6 +15,7 @@ score = 0
 
 xCsize = 2
 yCsize = yWmax/2 - 117/2
+xNPC_1 = 800
 
 #Imports
 bg = PhotoImage(file = "pixel-street.png")
@@ -37,14 +38,14 @@ player = Label(root, image = player_img, borderwidth=0)
 player.place(x = xCsize, y = yCsize)
 
 npc_1 = Label(root, image = npc_img, borderwidth=0)
-npc_1.place(x = 500, y = yCsize)
+npc_1.place(x = xNPC_1, y = yCsize)
 
-#Functions
+#Keys
 def movement_down(event):
-    player.place(x = xCsize, y = yCsize*2)
+    player.place(x = xCsize, y = yWmax - 160)
     print("d")
 def movement_up(event):
-    player.place(x = xCsize, y = yCsize/2)
+    player.place(x = xCsize, y = 45)
     print("q")
 def movement_mid(event):
     player.place(x = xCsize, y = yCsize)
@@ -56,6 +57,13 @@ root.bind("<s>",movement_mid)
 
 #Game start
 while Game == True:
+    
+# Execute tkinter
+    root.mainloop()
+#Pos
+    xNPC_1 = xNPC_1 - 0.001
+    print(xNPC_1)
+    npc_1.place(relx = xNPC_1, rely = yCsize)
 #Hitboxes
         #Check player x and y
     carXmin = player.winfo_rootx()
@@ -68,8 +76,7 @@ while Game == True:
     npcXmax = npc_1.winfo_rootx() + xCsize
     npcYmax = npc_1.winfo_rooty() + yCsize
         #Check colision
-    if carXmax >= npcXmin and carXmin <= npcXmax and carYmax >= npcYmin and carYmin <= npcYmax:
-        Game = False
-
-    # Execute tkinter
-    root.mainloop()
+    #if carXmax >= npcXmin and carXmin <= npcXmax and carYmax >= npcYmin and carYmin <= npcYmax:
+    #     Game = False
+    #     print("Game Over")
+    
