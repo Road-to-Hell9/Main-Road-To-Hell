8b530c24d45d2fd1a64826824daf9403b5d550ee
rosita <rosita.schrenk@gmail.com>
2022-05-19 17:18:09 +0200

    i tried to to the score thingy

--- start.py
+++ start.py
@@ -6,28 +6,47 @@ def open_main():
     start.destroy()
     import Main
 
+##def username():
+    value=username.get()
+    with open("fichier_1.txt" , "w" , encoding = "utf-8") as fichier :
+        fichier.write(value)
+
+def valider():
+    pass
+
+
+
+##canvas
 start = Tk()
 start.title("Main road to hell")
 start['bg']='white'
 start.geometry("1500x1000")
-
-
 img = PhotoImage(file='menubg.png')
 label = Label(image=img)
 label.place(x=0, y=0)
 
 
+username = str()
+nom = Entry(start, text="Your name", textvariable = username , width =10)
+nom.pack(padx=5,pady=5)
+
+
+
 label_fenetre = Label(start,text="Welcome",fg='black',font=("Times New Roman bold",70))
 label_fenetre.pack()
 
-bouton_jouer = Button(start,text = " wanna play? ",height = 5, width = 15,bg='green',fg='black', command = open_main )
+
+
+bouton_jouer = Button(start,text = " wanna play? ",height = 5, width = 15,bg='green',fg='black', command = open_main)
 bouton_jouer.pack(padx=10,pady=10)
 bouton_quit=Button(start, text="Quit",height = 5, width = 15,bg='red',fg='white', command=start.destroy)
 bouton_quit.pack(padx=1,pady=5)
 
-player = IntVar()
-nom = Entry(start, text="Your name", textvariable = player , width =10)
-nom.pack(padx=5,pady=5)
+##bouton_valider = Button(start,text = " sign me up ",bg='white',fg='black', command = valider)
+##with open("score.txt" , "r" , encoding = "utf-8") as fichier :
+##    for ligne in fichier :
+##        a=ligne
+##bouton_valider.pack(padx=0,pady=0)
 
 start.mainloop()
 
