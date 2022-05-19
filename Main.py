20a6b57985d35237eae672a06d6f8c688607f322
Christian Glaeser <Christian.glaeser05@gmail.com>
2022-05-19 17:22:46 +0200

    Main

--- Main.py
+++ Main.py
@@ -16,7 +16,9 @@ score = 0
 
 xCsize = 200
 yCsize = yWmax/2 - 117/2
-xNPC_1 = 800
+xNPC_1 = 1200
+xNPC_2 = 1200
+xNPC_3 = 1200
 
 #Imports
 bg = PhotoImage(file = "pixel-street.png")
@@ -41,6 +43,12 @@ player.place(x = 2, y = yCsize)
 npc_1 = Label(root, image = npc_img, borderwidth=0)
 npc_1.place(x = xNPC_1, y = yCsize)
 
+npc_2 = Label(root, image = npc_img, borderwidth=0)
+npc_2.place(x = xNPC_2, y = 45)
+
+npc_3 = Label(root, image = npc_img, borderwidth=0)
+npc_3.place(x = xNPC_3, y = yWmax - 160)
+
 #Keys
 def movement_down(event):
     player.place(x = 2, y = yWmax - 160)
@@ -57,7 +65,7 @@ root.bind("<d>",movement_down)
 root.bind("<s>",movement_mid)
 
 def main_game():
-    global xNPC_1, yCsize, root, Game, carXmax, carXmin, carYmax, carYmin, npcXmax, npcXmin, npcYmax, npcYmin
+    global xNPC_1, xNPC_2, xNPC_3, xCsize, root, Game
 #Game start
     if Game == True:
 #Hitboxes
@@ -67,16 +75,29 @@ def main_game():
         carXmax = player.winfo_rootx() + xCsize
         carYmax = player.winfo_rooty() + 117
         #Check Red Car x and y
-        npcXmin = npc_1.winfo_rootx()
-        npcYmin = npc_1.winfo_rooty()
-        npcXmax = npc_1.winfo_rootx() + xCsize
-        npcYmax = npc_1.winfo_rooty() + 117
+        npc1Xmin = npc_1.winfo_rootx()
+        npc1Ymin = npc_1.winfo_rooty()
+        npc1Xmax = npc_1.winfo_rootx() + xCsize
+        npc1Ymax = npc_1.winfo_rooty() + 117
+        
+        npc2Xmin = npc_2.winfo_rootx()
+        npc2Ymin = npc_2.winfo_rooty()
+        npc2Xmax = npc_2.winfo_rootx() + xCsize
+        npc2Ymax = npc_2.winfo_rooty() + 117
+        
+        npc3Xmin = npc_3.winfo_rootx()
+        npc3Ymin = npc_3.winfo_rooty()
+        npc3Xmax = npc_3.winfo_rootx() + xCsize
+        npc3Ymax = npc_3.winfo_rooty() + 117
         #Pos
         xNPC_1 = xNPC_1 - 3
         npc_1.place(x = xNPC_1, y = yCsize)
+        
+        xNPC_2 = xNPC_2 - 3
+        npc_2.place(x = xNPC_2, y = 45)
 
         #Check colision
-    if carXmax >= npcXmin and carXmin <= npcXmax and carYmax >= npcYmin and carYmin <= npcYmax:
+    if carXmax >= npc1Xmin and carXmin <= npc1Xmax and carYmax >= npc1Ymin and carYmin <= npc1Ymax or carXmax >= npc2Xmin and carXmin <= npc2Xmax and carYmax >= npc2Ymin and carYmin <= npc2Ymax or carXmax >= npc3Xmin and carXmin <= npc3Xmax and carYmax >= npc3Ymin and carYmin <= npc3Ymax:
         Game = False
         print("Game Over")
         
